e13366d0a14df8a337073de44e32159c0ebec10d
Антон Валиев <tosha.valiev.91@mail.ru>
2023-08-11 13:17:24 +0500

    Изменены модели Order и OrderDTO

--- SimplStudy/Models/DataBaseModels/Order.cs
+++ SimplStudy/Models/DataBaseModels/Order.cs
@@ -10,16 +10,6 @@ namespace SimplStudy.Models.DataBaseModels
         public int Id { get; set; }
 
         /// <summary>
-        /// Поле характеризующее общую стоимость всех товаров в заказе. 
-        /// </summary>
-        public int TotalPrice { get; set; }
-
-        /// <summary>
-        ///  Поле характеризующее общее количество всех товаров в заказе. 
-        /// </summary>
-        public int TotalCount { get; set; }
-
-        /// <summary>
         ///Навигационные свойство указывающее на список купленных товаров.
         /// </summary>
         public List<Item> Items { get; set; } = new List<Item>();
